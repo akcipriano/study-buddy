6cc318ca8e27535fd28122cca0cf589a0068a0c7
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-06-26 19:17:18 -0700

    Add three pages for each button in homepage

---
+++ client/components/studyBuddy.jsx
@@ -0,0 +1,18 @@
+import React from'react';
+
+class StudyBuddy extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div>
+        Study Buddy
+      </div>
+    )
+  }
+}
+
+export default StudyBuddy;
